335f443b30ef04748db3520ddd15841315a753c9
ymalkawi <malkawi.chicago@gmail.com>
2022-01-11 21:31:49 -0600

    Calculator Project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function insert(num){
+    $('.calc-display').val($('.calc-display').val() + num);
+}
+function eql(){
+    $('.calc-display').val(eval($('.calc-display').val()));
+}
+function c(){
+    $('.calc-display').val('');
+}
+function del(){
+value = $('.calc-display').val();
+$('.calc-display').val(value.substring(0, value.length -1));
+
+}
+                
